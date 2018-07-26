ff00a813d674e461729120efad6e05e8b5b0795d
igoratf <igor.atf@gmail.com>
2018-07-26 11:11:23 -0300

    add navigation guards

--- Frontend/pre-matricula/src/router.js
+++ Frontend/pre-matricula/src/router.js
@@ -5,30 +5,49 @@ import Login from './components/Login.vue'
 import CadastroAlunos from './components/CadastroAlunos.vue'
 import About from './views/About.vue'
 
+import firebase from 'firebase'
+
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
+      path: '*',
+      redirect: '/login'
+    },
+    {
       path: '/',
-      redirect:'/login',
-      name: 'home',
-      component: Home
+      redirect:'/about'
     },
     {
       path:'/login',
       name: 'login',
-      component: Login
+      component: Login,
     },
     {
-      path:'/cadastro',
+      path:'/cadastro-alunos',
       name:'cadastro-alunos',
-      component: CadastroAlunos
+      component: CadastroAlunos,
+      meta: {requiresAuth: true}
     },
     {
       path: '/about',
       name: 'about',
-      component: About
+      component: About,
+      meta: {requiresAuth: true}
     },
   ]
 })
+
+
+router.beforeEach((to, from, next) => {
+  let currentUser = firebase.auth().currentUser;
+  let requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+
+  if (requiresAuth && !currentUser) next('login')
+  else if (!requiresAuth && currentUser) next('cadastro-aluno')
+  else next()
+})
+
+
+export default router
