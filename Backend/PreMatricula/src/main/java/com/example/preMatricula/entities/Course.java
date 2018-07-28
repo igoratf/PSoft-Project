2369ba9e7cbb592bfb759bb7d9462ebd23a14d34
Icaro-Lima <icaro.lima@ccc.ufcg.edu.br>
2018-07-27 23:52:29 -0300

    ADD 'Course' e 'Student'

--- Backend/PreMatricula/src/main/java/com/example/preMatricula/entities/Course.java
+++ Backend/PreMatricula/src/main/java/com/example/preMatricula/entities/Course.java
@@ -1,5 +1,5 @@
 package com.example.preMatricula.entities;
 
 public class Course {
-
+	
 }
--- Backend/PreMatricula/src/main/java/com/example/preMatricula/entities/Student.java
+++ Backend/PreMatricula/src/main/java/com/example/preMatricula/entities/Student.java
@@ -1,5 +1,42 @@
 package com.example.preMatricula.entities;
 
-public class Student {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Student {
+	
+	private String email;
+	private Long registration;
+	private Short period;
+	private List<Discipline> enrolledDisciplines;
+	
+	public Student(String email, Long registration, Short period) {
+		super();
+		this.email = email;
+		this.registration = registration;
+		this.period = period;
+		
+		this.enrolledDisciplines = new ArrayList<>();
+	}
+
+	public Long getRegistration() {
+		return registration;
+	}
+
+	public void setRegistration(Long registration) {
+		this.registration = registration;
+	}
+
+	public Short getPeriod() {
+		return period;
+	}
+
+	public void setPeriod(Short period) {
+		this.period = period;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+	
 }
