016705858d94c2b66af155dd34d1e3ccfa8264ba
Icaro-Lima <icaro.lima@ccc.ufcg.edu.br>
2018-07-28 05:18:16 -0300

    implementa base para 'Course'

--- Backend/PreMatricula/src/main/java/com/example/preMatricula/entities/Course.java
+++ Backend/PreMatricula/src/main/java/com/example/preMatricula/entities/Course.java
@@ -1,5 +1,23 @@
 package com.example.preMatricula.entities;
 
+import java.util.Map;
+
 public class Course {
+	
+	private Map<Long, Student> students;
+	private Map<Long, Discipline> disciplines;
+	
+	public Course() {
+		super();
+	}
+	
+	public void enrollStudentInDiscipline(Student student, Discipline discipline) {
+		if (student == null) {
+			throw new NullPointerException("Estudent não pode ser null");
+		}
+		
+		
+		
+	}
 	
 }
