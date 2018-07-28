641c1216e1a2b6b059c20500e75975c63efebb11
Icaro-Lima <icaro.lima@ccc.ufcg.edu.br>
2018-07-28 05:52:36 -0300

    melhora exceções lançadas por 'Course.enrollStudentInDiscipline'

--- Backend/PreMatricula/src/main/java/com/example/preMatricula/entities/Course.java
+++ Backend/PreMatricula/src/main/java/com/example/preMatricula/entities/Course.java
@@ -4,34 +4,36 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Course {
-	
+
 	private Map<Long, Student> students;
 	private Map<Long, Discipline> disciplines;
-	
+
 	public Course() {
 		super();
-		
+
 		this.students = new HashMap<>();
 		this.disciplines = new HashMap<>();
 	}
-	
+
 	/**
 	 * Matricula um aluno específico em uma disciplina específica.
+	 * 
 	 * @param studentRegistration A matrícula do aluno.
-	 * @param disciplineCode O código da disciplina.
+	 * @param disciplineCode      O código da disciplina.
 	 */
 	public void enrollStudentInDiscipline(Long studentRegistration, Long disciplineCode) {
 		Student student = this.students.get(studentRegistration);
 		Discipline discipline = this.disciplines.get(disciplineCode);
-		
+
 		if (student == null) {
-			throw new NullStudentException();
+			throw new IllegalArgumentException(
+					"O estudante de matrícula " + studentRegistration + " não existe no sistema.");
 		} else if (discipline == null) {
-			throw new NullDisciplineException();
+			throw new IllegalArgumentException("A disciplina de código " + disciplineCode + " não existe no sistema.");
 		}
-		
+
 		student.enrollInDiscipline(discipline);
 		discipline.enrollStudent(student);
 	}
-	
+
 }
