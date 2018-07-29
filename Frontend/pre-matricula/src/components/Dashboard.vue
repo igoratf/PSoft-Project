f41cebb90aef570898398999b1970a1679182060
igoratf <igor.atf@gmail.com>
2018-07-29 15:35:21 -0300

    add courses checkbox binding

--- Frontend/pre-matricula/src/components/Dashboard.vue
+++ Frontend/pre-matricula/src/components/Dashboard.vue
@@ -1,13 +1,15 @@
-<template lang="html">
+/* <template lang="html">
 
   <section class="dashboard">
     <MenuPreMat />
 
     <div class="container animated zoomIn faster">
       <h1> Lista de disciplinas cadastradas</h1>
+      <form @submit.prevent="submit">
     <table class="table table-hover">
   <thead>
     <tr>
+      <th scope="col"></th>
       <th scope="col">Período</th>
       <th scope="col">Código</th>
       <th scope="col">Disciplina</th>
@@ -17,15 +19,23 @@
     </tr>
   </thead>
   <tbody>
-    <tr v-for="list in listTest">
-      <th scope="row">{{list.item}}</th>
+    <tr v-for="list in listTest" :key="list.code">
+      <input class="course-checkbox" type="checkbox" :value="list" v-model="checked">
+      <th scope="row">{{list.period}}</th>
+      <td>{{list.code}}</td>
       <td>{{list.name}}</td>
-      <td>{{list.prop}}</td>
-      <td>{{list.yes}}</td>
+      <td>{{list.credit}}</td>
+      <td>{{list.workload}}</td>
+      <td>{{list.coursePlan}}</td>
     </tr>
   </tbody>
 </table>
+<hr>
+<button type="submit" class="btn btn-primary">Concluir</button>
+</form>
 </div>
+
+{{checked}}
   </section>
 
 </template>
@@ -45,20 +55,20 @@ export default {
     return {
       listTest: [
         {
-          'item':'oi',
-          'name':'dois',
-          'prop':'tres',
-          'yes': 'no'
-        }]
-    }
+          period: 1,
+          code: "18290380123",
+          name: "Programação I",
+          credit: 4,
+          workload: 60,
+          coursePlan: "Ambas"
+        }
+      ],
+      checked: []
+    };
   },
   methods: {
-    checkCurrentLogin() {
-      if (AuthService.checkCurrentLogin()) {
-        this.$router.replace(this.$route.query.redirect || "/dashboard");
-      } else {
-        this.$router.replace("/login");
-      }
+    submit() {
+
     }
   },
   computed: {},
@@ -81,7 +91,12 @@ export default {
   margin-top: 5%;
 }
 
+.course-checkbox {
+  margin-top: 18px;
+}
+
 h1 {
   margin-bottom: 2%;
 }
 </style>
+ */
