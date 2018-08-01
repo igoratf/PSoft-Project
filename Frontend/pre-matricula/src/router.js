10d591ed8962c60b2345fb2a0c34f5c813545001
igoratf <igor.atf@gmail.com>
2018-08-01 12:40:06 -0300

    validates login before enter route

--- Frontend/pre-matricula/src/router.js
+++ Frontend/pre-matricula/src/router.js
@@ -24,7 +24,7 @@ const router = new Router({
       component: Login,
       beforeEnter: (to, from, next) => {
         let currentUser = AuthService.getCurrentUser();
-        if (currentUser.role) {
+        if (currentUser && currentUser.role) {
           next("dashboard")
         } else {
           next()
